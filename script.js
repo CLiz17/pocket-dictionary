8f325f88242ca390778d99deade8b6a64cea2b3d
Liz <cliz@pop-os.localdomain>
2023-11-10 16:21:26 +0530

    link css and js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
-const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", () => {
@@ -12,26 +11,18 @@ btn.addEventListener("click", () => {
             result.innerHTML = `
             <div class="word">
                     <h3>${inpWord}</h3>
-                    <button onclick="playSound()">
-                        <i class="fas fa-volume-up"></i>
-                    </button>
                 </div>
                 <div class="details">
-                    <p>${data[0].meanings[0].partOfSpeech}</p>
-                    <p>/${data[0].phonetic}/</p>
+                    <p>Parts of Speech : ${data[0].meanings[0].partOfSpeech}</p>
                 </div>
                 <p class="word-meaning">
-                   ${data[0].meanings[0].definitions[0].definition}
+                   Meaning : ${data[0].meanings[0].definitions[0].definition}
                 </p>
                 <p class="word-example">
-                    ${data[0].meanings[0].definitions[0].example || ""}
+                    Example : ${data[0].meanings[0].definitions[0].example || ""}
                 </p>`;
-            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
         })
         .catch(() => {
-            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+            result.innerHTML = `<p class="error">Couldn't Find The Word</p>`;
         });
 });
-function playSound() {
-    sound.play();
-}
